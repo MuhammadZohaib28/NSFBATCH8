ff6402d18f56d7efc055fd387d6afb622d192921
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-05-05 22:02:33 +0500

    JS DAY 1

---
+++ javascript/day1.js
@@ -0,0 +1,36 @@
+console.log("Hello World");
+
+//THERE ARE 3 TYPES OF VARIABLES
+// 1 - var
+// 2 - let
+// 3 - const
+
+var userName = "Bisma";
+var userName = "Hamza";
+console.log(userName);
+
+let userNameOne = "Hammad";
+let userNameTwo = "Danish";
+console.log(userNameTwo);
+console.log(userNameOne);
+
+const userName1 = "Zohaib";
+
+console.log(userName1);
+
+const newName = "Ahsan";
+
+{
+    console.log(newName, "YEH BLOCK SCOPE SAY ARAHA HAI");
+}
+
+
+
+//DATA TYPES IN JS
+// 1 - number
+// 2 - string
+// 3 - object
+// 4 - arrays
+// 5 - boolean
+
+
